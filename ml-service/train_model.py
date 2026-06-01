6197c7ddf50cb9a1599680ef5256e3b9bf632d1f
TNaveen-24 <naveenterukula@gmail.com>
2026-06-02 02:58:21 +0530

    Fix NameError: import numpy as np

--- ml-service/train_model.py
+++ ml-service/train_model.py
@@ -8,6 +8,7 @@ import os
 import sys
 import ast
 import pandas as pd
+import numpy as np
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
